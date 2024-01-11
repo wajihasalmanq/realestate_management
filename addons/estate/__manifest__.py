b72c818f0eca6e9c25dc4d347946c9eb4842b445
wajihasalmanq <wajihasalmanpk@gmail.com>
2024-01-11 22:01:47 +0500

    project

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+  'name': "Real-Estate Management",
+  'version': '16',
+  'depends': ['base'],
+  'author': "wajiha salman",
+  'category': 'Category',
+  'description': """
+  This is a test module of Real-Estate Management!
+  Written for the Odoo Quickstart Tutorial.
+  """,
+  # data files always loaded at installation
+  'data': [
+  ],
+  'installable': True,
+  'auto_install': False,
+  'application': False,
+}
